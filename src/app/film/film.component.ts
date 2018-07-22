a4b4820040dcd8d9a1c37505ebaa015842c4ab8c
Tom Broderick <tombroderick@gmail.com>
2018-07-22 10:19:03 -0600

    requirement iv completed

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class FilmComponent implements OnInit {
   selected = null;
   newFilm: Film = new Film();
+  editFilm = null;
   films = [
     new Film(
       1,
@@ -30,8 +31,39 @@ export class FilmComponent implements OnInit {
   }
 
   addFilm() {
+    this.newFilm.id = this.generateId();
     this.films.push(this.newFilm);
     this.newFilm = new Film();
+  }
+
+  setEditFilm() {
+    console.log(this.selected);
+    this.editFilm = Object.assign({}, this.selected);
+    console.log(this.editFilm);
+  }
+
+  generateId() {
+    return this.films[this.films.length - 1].id + 1;
+  }
+
+  updateFilm(film) {
+    const id = film.id - 1;
+    this.films[id].title = film.title;
+    this.films[id].description = film.description;
+    this.films[id].releaseYear = film.releaseYear;
+    this.films[id].rentalRate = film.rentalRate;
+    this.films[id].length = film.length;
+    this.films[id].replacementCost = film.replacementCost;
+    this.films[id].rating = film.rating;
+    this.films[id].language = film.language;
+    this.editFilm = null;
+
+  }
+
+  deleteFilm() {
+    const id = this.selected.id - 1;
+    this.films.splice(id, 1);
+    this.selected = null;
   }
 
   constructor() {}
